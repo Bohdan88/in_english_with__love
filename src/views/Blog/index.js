cafc0f998005aa46e399ac7df106f5a129a5f54f
Bohdan <bohdan.martyniuk1488@gmail.com>
2020-04-28 20:24:36 -0400

    db to blog

--- src/views/Blog/index.js
+++ src/views/Blog/index.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { withAuthorization } from "../Session";
+import { withFirebase } from "../Firebase";
+import { compose } from "recompose";
 
 class Blog extends Component {
-    render() {
-        return (
-            <div>
-                Blog
-            </div>
-        )
-    }
+  state = {
+    loading: false,
+    posts: [],
+  };
+
+  componentDidMount() {
+    this.listener = this.props.firebase.posts().on("value", (snapshot) => {
+      const postsObject = snapshot.val();
+
+      const postsList = Object.keys(postsObject).map((key) => ({
+        ...postsObject[key],
+        uid: key,
+      }));
+
+      this.setState({
+        posts: postsList,
+        loading: false,
+      });
+    });
+  }
+
+  componentWillMount() {
+    // this.listener();
+  }
+
+  render() {
+      console.log(this.state)
+    return <div>Blog</div>;
+  }
 }
 
-export default Blog;
+// const condition = (authUser) => authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default withFirebase(Blog);
+
+// export default Blog;
